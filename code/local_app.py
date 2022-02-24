ff7a9df17a600e213b81ccf82c5932d82b4d6f89
kluu <k.luu@Live.com>
2022-02-24 16:22:06 -0500

    update

--- code/local_app.py
+++ code/local_app.py
@@ -25,6 +25,21 @@ import torch
 import transformers
 from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
 
+# Configurations -- page theme & warning suppressions
+st.set_page_config(layout="wide")
+
+st.config.set_option('theme.primaryColor', '#FF4B4B')
+st.config.set_option('theme.backgroundColor', '#0E1117')
+st.config.set_option('theme.secondaryBackgroundColor', '#262730')
+st.config.set_option('theme.textColor', '#FAFAFA')
+st.config.set_option('theme.font', 'monospace')
+
+st.config.set_option('client.showErrorDetails', False)
+st.config.set_option('global.showWarningOnDirectExecution', False)
+st.config.set_option('global.suppressDeprecationWarnings', True)
+st.config.set_option('global.disableWatchdogWarning', True)
+
+
 ##################################################
 # Text cleaner
 ##################################################
@@ -47,7 +62,11 @@ def cleaner(text):
 	text = re.sub("not ", " NOT", text)
 	text = re.sub("no ", " NO", text)
 
-	# Remove non-alphabets
+	# Contractions
+	text = re.sub(r"\'m", " am", text)
+	text = re.sub(r"\'s", " is", text)
+
+	# Remove non-alphanumeric characters
 	text = re.sub(r"[^A-Za-z0-9^,!?.\/'+]", " ", text)
 
 	# Extra cleaning beyond bs4
@@ -96,18 +115,67 @@ def predict(user_input):
 	padded = pad_sequences(tokenizer.texts_to_sequences([cleaned_input]), 300)
 	pred = model.predict(padded)[0][0]
 	
-	st.write(f'{round(pred*100,2)}% POSITIVE')
-	st.write(f'{round((1-pred)*100,2)}% NEGATIVE')
+	results = {'TEXT':[user_input],
+		'TEXT ANALYZED':[cleaned_input],
+		'POSITIVE %':[round(pred*100,2)],
+		'NEGATIVE %':[round((1-pred)*100,2)]
+		}
+	
+	results_df = pd.DataFrame(results)
+	
+	# Color mapping 
+	if pred > 0.7:
+		fill_color = ['#b9ffd6']
+	elif pred > 0.3:
+		fill_color = ['#ffd6b9']
+	else:
+		fill_color = ['#ffb9e2']
+		
+	# Table output	
+	table = go.Figure(data=[go.Table(
+		header=dict(values=[f"<b>{col.upper()}</b>" for col in list(results_df.columns)],
+			fill_color='#b9e2ff',
+			font=dict(color='black', family="monospace", size=14),
+			align='center'),
+		cells=dict(values=[results_df.TEXT, results_df['TEXT ANALYZED'], results_df['POSITIVE %'], results_df['NEGATIVE %']],
+		       fill_color = fill_color,
+		       font=dict(color='black', family="monospace", size=14),
+		       align='left'))
+		])
+	table.update_layout(width=1500)
+	st.plotly_chart(table, use_container_width=True)
 	
 	if pred > 0.7:
-		st.write('Conclusion: POSITIVE EXPERIENCE')
-		st.write("This client has experience an overall positive. Continue to deliver similar products and/or solutions that align with the positives in this review. Try out our in-depth analyzer to further understand your client experiences.")
+		image = """<img src="https://cdn.shopify.com/s/files/1/1061/1924/files/Slightly_Smiling_Face_Emoji.png?9898922749706957214"
+			alt="happy emoji"
+			width="300"
+			height="300">
+			"""
+		st.markdown(image, unsafe_allow_html=True)
+		
+		st.write('Conclusion: __POSITIVE EXPERIENCE__')
+		st.write("This client had an overall __positive__ experience. Continue to deliver similar products and/or solutions that align with the positives in this review. Try out our in-depth analyzer to further understand your client experiences. Feel free to reach out to us at improvements@bestservice.ca for 1-on-1 insights.")
+	
 	elif pred > 0.3:
-		st.write('Conclusion: NEUTRAL EXPERIENCE')
+		image = """<img src="https://cdn.shopify.com/s/files/1/1061/1924/products/Neutral_Face_Emoji_large.png?v=1571606037"
+			alt="sad emoji"
+			width="300"
+			height="300">
+			"""
+		st.markdown(image, unsafe_allow_html=True)
+	
+		st.write('Conclusion: __NEUTRAL EXPERIENCE__')
 		st.write("Although this is not a negative experience, let's discuss about this experience to understand the areas of improvements. Please refer to our consultants at improvements@bestservice.ca. We would like to work with you in developing the best solutions to achieve positive results.")		
 	else:
-		st.write('Conclusion: NEGATIVE EXPERIENCE')
-		st.write("Negative experience are always a great learning opportunity. Let's figure out a solution that can turn this negative into a positive for the future. Please refer to our consultants at improvements@bestservice.ca. We're more than happy to assist you with mitigating negative experiences!")
+		image = """<img src="https://cdn.shopify.com/s/files/1/1061/1924/files/Sad_Face_Emoji.png?9898922749706957214"
+			alt="sad emoji"
+			width="300"
+			height="300">
+			"""
+		st.markdown(image, unsafe_allow_html=True)
+		
+		st.write('Conclusion: __NEGATIVE EXPERIENCE__')
+		st.write("__Negative__ experiences are always a great learning opportunity. Let's figure out a solution that can turn this negative into a positive for the future. Please refer to our consultants at improvements@bestservice.ca. We're more than happy to assist you with mitigating negative experiences!")
 	
 def length(user_input):
 	words = user_input.split()
@@ -161,43 +229,57 @@ def load_upload():
 ########################################################################################################################################################################################################
 # WEB-APP
 ########################################################################################################################################################################################################
-st.set_page_config(layout="wide")
-page = st.sidebar.selectbox('Select page', ['Please select', 'Simple Analyzer','In-Depth Analyzer'])
+# Front-page
+
+
+#st.title("PRIMEMINDS CONSULTING")
+page = st.sidebar.selectbox('Select an analyzer', ['Please select', 'Simple Analyzer','In-Depth Analyzer'])
 
 if page == 'Please select':
-	st.title("REVIEW ANALYSIS CONSULTATION SERVICES")
-	st.header("Accelerate your growth.")
-	st.markdown("With your client reviews, we can stategically develop innovative solutions to help accelerate positive changes & unlock your vision.")
+	# Front-page title
+	title = """<h1 style= 'color:white;'>
+		<b>PRIMEMINDS CONSULTING</b> 
+		<span style='color:#A0A0A0; font-size:10px'>Product and solutions consulting services</span>
+		</h1>
+		"""
+	st.markdown(title, unsafe_allow_html=True)
 	
+	# Front-page company statement
+	col1, col2 = st.columns(2)
+	with col1:
+		st.subheader("Accelerate your growth.")
+		st.write("With your client reviews, we can stategically develop innovative solutions to help accelerate positive changes & unlock your vision.")
+		
 ##################################################
 # PAGE 1 - SIMPLE, SINGLE REVIEW INPUT
 ##################################################
-# Single review page
 elif page == 'Simple Analyzer':
-	st.title("REVIEW ANALYSIS CONSULTATION SERVICES")
+	st.title("PRIMEMINDS CONSULTING")
 	
 	# Load pre-train tokenizer and neural network model (highest accuracy)
 	tokenizer = load_tokenizer()
 	model = load_cnn()
 	
 	st.header('Simple Analyzer')
-	st.markdown('Enter a review to analyze!')
-	review = st.text_area('15 words minimum')
+	review = st.text_area('Enter a review to analyze')
 	
-	if not review:
-		st.warning('Please enter a review.')
+	if length(review) < 15:
+		st.warning('Please enter a 15 words minimum review.')
+		st.stop()
 	else:
-		if length(review) < 15:
-			st.warning('Please enter a minimum 15 word review.')
-		else:
+		try:
 			predict(review)
+		except:
+			st.error("We've encountered an error. Try again.")
+			st.stop()
 
 ##################################################
 # PAGE 2 - COMPLEX, REVIEW COLLECTION INPUT
 ##################################################
 elif page == 'In-Depth Analyzer':
-	st.title("REVIEW ANALYSIS CONSULTATION SERVICES")
+	st.title("PRIMEMINDS CONSULTING")
 	
+	# Load pre-trained model
 	classifier = load_bart()
 
 	st.header('In-Depth Analyzer')
@@ -211,106 +293,119 @@ elif page == 'In-Depth Analyzer':
 				mime='text/csv')
 	
 	st.markdown("For efficiency purposes, work with 50 reviews at a time.")
+	
+	# Uploader to accept csv of reviews
 	upload = st.file_uploader('Upload a file')
 	
+	# Preprocessing & cleaning
 	if not upload:
 		st.warning('Please upload a .csv file')
 	else:
 		st.spinner(f'You have selected {upload.name}. Loading file...')
 		stqdm.pandas() # initializing progress_apply function
 		
+		# Clean
 		try:
 			df = load_upload()
 			st.success("Cleaned & stored!")
 		except:
-			st.write("Your file is not formatted correctly. \n Please ensure that there's a column named 'text' - all lower-cased")
-			
+			st.error("Your file does not meet the criteria. Please ensure it's a .csv file with a 'text' column.")
+			st.stop()
+		
+		# Random sample selection	
 		sample_size = st.selectbox('Select sample size', ['Please select', 5, 50, 100, 200, 400, 500, 'ALL'])
 		
-		################################################## 
-		# SENTIMENT DISTRIBUTIONS
-		##################################################
-		st.header("Sentiment Distribution")
-		
 		if sample_size == 'Please select':
-			st.error('Please select a sample size.')
+			st.warning('Please select a sample size.')
+			st.stop()
 		elif (sample_size == 'ALL') or (sample_size >= len(df)):
 			df2 = df.copy()		
 		elif type(sample_size) == int:
 			df2 = df.sample(n=sample_size, ignore_index=True)
-			
-		if len(df2) > 0:
+		
+		# Sentiment prediction based on pre-trained BART	
+		try:
 			with st.spinner('Analyzing sentiment...'):
 				df2['sentiment'] = df2.text.progress_apply(sentiment_pred) # predict sentiment	
 			
 			frequency = df2.sentiment.value_counts() # extract sentiment value counts -- which is in descending order
 			labels = list(frequency.index) # sentiment labels
 			values = list(frequency) # frequencies of each sentiment
-		else:
-			labels = []
-			values = []
-			st.error('It seems no analysis was achieved with your uploaded file. Try another file.')
-		
-		# 2 Columns
-		pie, reviews = st.columns(2)
-		
-		# Column 1 - donut-pie sentiment distirbution
-		with pie:	
-			# donut pie-chart
-			donut = go.Figure(data=[go.Pie(labels=[l.upper() for l in labels], values=values, hole=.3)])
-			donut.update_traces(marker = dict(colors=['#b9bfff','#fff9b9','#ffb9bf']))
-			st.plotly_chart(donut, use_container_width=True)
-				
-			if labels[0] == 'positive':
-				st.write("The majority of your reviews are __positive__. Let's identify common trends to consitently maintain positivity.")
-			elif labels[0] == 'negative':
-				st.write("The majority of your reviews are __negative__. Let's identify areas of opportunities for improvement.")
-			elif labels[0] == 'neutral':
-				st.write("The majority of your reviews are __neutral__. Let's observe the reviews to figure out how we can improve the positives while minimizing the negatives.")
+		except:
+			st.error('It seems no analysis was achieved with your selection. Try re-selecting the sample size or re-uploading your file.')
+			st.stop()
 		
 		################################################## 
-		# LABELED DATAFRAME
+		# SENTIMENT DISTRIBUTIONS & LABELED DATAFRAME
 		##################################################
+		st.header("Sentiment Distribution")
 		
-		# Color mapping created
-		color_map = {'negative':'#ffb9e2', 'positive':'#b9ffd6', 'neutral':'#ffd6b9'}
-		mapped_colors = list(df2["sentiment"].map(color_map))
-		fill_color = [mapped_colors for _ in range(len(df2.columns))]
+		# 2 Columns - donut-pie chart & labeled reviews
+		pie, reviews = st.columns(2)
+		
+		try:
+			# Column 1 - donut-pie sentiment distribution
+			with pie:	
+				# donut pie-chart
+				donut = go.Figure(data=[go.Pie(labels=[l.upper() for l in labels], values=values, hole=.3)])
+				donut.update_traces(marker = dict(colors=['#b9bfff','#fff9b9','#ffb9bf']))
+				st.plotly_chart(donut, use_container_width=True)
+					
+				if labels[0] == 'positive':
+					st.write("The majority of your reviews are __positive__. Let's identify common trends to consitently maintain positivity.")
+				elif labels[0] == 'negative':
+					st.write("The majority of your reviews are __negative__. Let's identify areas of opportunities for improvement.")
+				elif labels[0] == 'neutral':
+					st.write("The majority of your reviews are __neutral__. Let's observe the reviews to figure out how we can improve the positives while minimizing the negatives.")	
 
-		# Column 2 - reviews with predicted sentiment
-		with reviews:
-			table = go.Figure(data=[go.Table(
-				header=dict(values=[f"<b>{col.upper()}</b>" for col in list(df2.columns)],
-					fill_color='#b9e2ff',
-					align='center'),
-				cells=dict(values=[df2.text, df2.sentiment],
-				       fill_color = fill_color,
-				       align='left'))
-				])
-			table.update_layout(width=1500, height=750)
-			st.plotly_chart(table, use_container_width=True)
+			# Column 2 - reviews with predicted sentiment table
+			with reviews:
+				# Color mapping created
+				color_map = {'negative':'#ffb9e2', 'positive':'#b9ffd6', 'neutral':'#ffd6b9'}
+				mapped_colors = list(df2["sentiment"].map(color_map))
+				fill_color = [mapped_colors for _ in range(len(df2.columns))]
+				table = go.Figure(data=[go.Table(
+					header=dict(values=[f"<b>{col.upper()}</b>" for col in list(df2.columns)],
+						fill_color='#b9e2ff',
+						font=dict(color='black', family="monospace", size=14),
+						align='center'),
+					cells=dict(values=[df2.text, df2.sentiment],
+					       fill_color = fill_color,
+					       font=dict(color='black', family="monospace", size=14),
+					       align='left'))
+					])
+				table.update_layout(width=1500, height=750)
+				st.plotly_chart(table, use_container_width=True)
+		except:
+			st.warning('Pending analysis')
+			st.stop()	
 
 		
 		################################################## 
 		# CHARACTER & COUNT DISTRIBUTIONS
 		##################################################
 		st.header("Character and Word Count Distributions")
-		df3 = df.copy()
 		char, word = st.columns(2)
 		
-		with char:	
-			# Character count distribution	
-			df3['char_count'] = df3.text.str.len()
-			fig = px.histogram(df3, x='char_count', color_discrete_sequence=['#ffb9bf'])
-			st.plotly_chart(fig, use_container_width=True)
-			st.write(f"Most reviews have around a __{df3.char_count.mode()[0]}__ character count, but the overall average is about __{round(df3.char_count.mean())}__ characters per review.")
-		
-		with word:
-			# Word count distribution
-			df3['word_count'] = df3.text.str.split().apply(len)
-			fig2 = px.histogram(df3, x='word_count', color_discrete_sequence=['#b9bfff'])
-			st.plotly_chart(fig2, use_container_width=True)
-			st.write(f"Most reviews have around a __{df3.word_count.mode()[0]}__ word count, but the overall average is about of __{round(df3.word_count.mean())}__ words per review.")
+		try:
+			df3 = df.copy()
+			with char:	
+				# Character count distribution	
+				df3['char_count'] = df3.text.str.len()
+				fig = px.histogram(df3, x='char_count', color_discrete_sequence=['#ffb9bf'])
+				st.plotly_chart(fig, use_container_width=True)
+				st.write(f"Most reviews have around a __{df3.char_count.mode()[0]}__ character count, but the overall average is about __{round(df3.char_count.mean())}__ characters per review.")
+			
+			with word:
+				# Word count distribution
+				df3['word_count'] = df3.text.str.split().apply(len)
+				fig2 = px.histogram(df3, x='word_count', color_discrete_sequence=['#b9bfff'])
+				st.plotly_chart(fig2, use_container_width=True)
+				st.write(f"Most reviews have around a __{df3.word_count.mode()[0]}__ word count, but the overall average is about of __{round(df3.word_count.mean())}__ words per review.")
+				
+		except:
+			st.warning('Pending analysis')
+			st.stop()
 		 
 		 
 			
