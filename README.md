f8ca134c0dd0e0cd3317e5930b9b60891a8f6c08
kluu <k.luu@Live.com>
2022-02-23 19:47:10 -0500

    Upload

--- README.md
+++ README.md
@@ -1,1 +1,170 @@
-# sentiment_analysis
+#  PROJECT 3: WEB APIs & NLP
+## *PREDICTING WHICH SUBREDDIT A POST IS FROM (r/pcgaming vs. r/consoles)*
+***BY: KEVIN LUU***
+
+## EXECUTIVE SUMMARY
+
+### PROBLEM STATEMENT
+Throughout the pandemic, the video gaming industry experienced a boom and thrived. Due to the restrictions, people are at bored at home and have a lot more spare time with nothing to do. This is where gaming become a way to pass time. As for others, gaming helped individuals with avoiding loneliness, allowing them to remain connected with friends.<sup>1</sup>
+
+Looking at the numbers: Microsoft's Game Pass (a Netflix for gaming) hit 10-million subscribers; Nintendo's Switch console sales went up by 24%; Twitch (a live-streaming platform full of gamers) had an increase of 101% in hours of live-streams watch hitting 1.6 billions hours of content watched per month; and Facebook Gaming had the largest increase of 238% in growth.<sup>2,3</sup>
+
+The question that always comes up within the gaming world is: **PC or Console?** Which is better?
+I personally would choose PC, but regardless of my preference, the question is the source for which 2 subreddits were selected for this project.
+
+The goal of the project is to extract posts from two subreddits and undergo Natural Language Processing (NLP) to create & train models that can distinguish which subreddit a post comes from. The two subreddits selected are:
+1. **PC Gaming** (https://www.reddit.com/r/pcgaming/)
+2. **Console Gaming** (https://www.reddit.com/r/consoles/
+
+*Citations:*
+<br><sup>1</sup> - https://www.theglobeandmail.com/life/article-why-the-pandemic-videogame-boom-is-great-for-canada/
+<br><sup>2</sup> - https://www.washingtonpost.com/video-games/2020/05/12/video-game-industry-coronavirus/
+<br><sup>3</sup> - https://www.theverge.com/2020/5/13/21257227/coronavirus-streamelements-arsenalgg-twitch-youtube-livestream-numbers
+
+
+### DATA
+Due to difficulties of bypassing the 999 post limit with the base [Pushshift.io API](https://github.com/pushshift/api), the data was collected through the usage of [Pushshift Multithreading API Wrapper (PMAW)](https://pypi.org/project/pmaw/0.0.2/). Setting the limit to 10,000 posts, I was able to collect:
+- 10000 submissions from r/pcgaming
+- 5461 submissions from r/consoles
+
+Information was collected from 3 parameter tags:
+- 'subreddit' -- the name of the subreddit the post comes from
+- 'title' -- the title of the post
+- 'selftext' -- the text contents of the post
+
+
+### METHODOLOGY
+- Data Cleaning
+    - label encoding 'subreddit' feature
+    - replaced null/\[deleted\]/\[removed\] posts
+    - combined 'title' and 'selftext' into one column: 'text'
+- Experimental Data Analysis (EDA)
+    - additional text cleaning
+        - (lowercase, removal of URL elements, retained letters only)
+    - NLP vectorization to observe top 20 frequent words
+        - Count Vectorization
+        - TF-IDF Vectorization
+- Modeling Training & Implementation
+    - custom stop words list created for tuning
+    - initial observation of Logistic Regression
+    - model selection based on cross-validation scores of various models
+    - top 2 models chosen after hypertuning selected models
+- Feature Importances
+    - based on top 2 models selected
+
+**Data Cleaning:**
+<br>The feature 'selftext' was the only one that had missing values. This was due to the post being an image-only post without any text. Removing these posts was not the option as the title of the posts still contained useful information. Therefore, all null posts along with all posts that only had "[deleted]" or "[removed]" were filled with a blank space. 
+<br>Since the title also contained useful information, a new column called 'text' was created as a combination and replacement of the 'title' and 'selftext' features
+<br>The 'subreddit' feature contained 'pcgaming' or 'console' -- therefore, was handled with label-encoding:
+- pcgaming = 1
+- console = 0
+<br>The cleaned data was then exported into a new .csv file as it no longer has any null values, the text information has been consolidated, and the target feature is now numerical. The data was ready for natural language processing & modeling.
+
+**Data Dictionary:**
+|Feature|Dataset|Description|
+|:---|:---|:---|
+|subreddit|submissions_clean.csv|The subreddit that the post belongs to (0 = r/consoles, 1 = r/pcgaming)|
+|text|submissions_clean.csv|The consolidated text of the title and contents of the post.|
+
+**EDA:**
+<br>With the use of Count Vectorization on the consolidated text, the initial look at the top 20 features indicated that there were many commonly shared words between both subreddits & that there were URL elements that needed to be omitted.
+<br>This resulted in additional text cleaning to remove all URL elements, remove non-letter characters, and lower-case everything.
+<br>Upon a second look at the top 20 words after the text cleaning, additional commonly shared words were discovered. Therefore, a custom stop words list that included the commonly shared words was created.
+<br>Here's a visual look at the top 20 words from each subreddit:
+![top20console](images/top20_console.png "Top 20 words in r/consoles")
+![top20pc](images/top20_pc.png "Top 20 words in r/pcgaming")
+
+**Modeling:**
+<br>In order to select the best model for training, the 5-fold cross validation scores all 14 combinations of 7 models + 2 vectorizers (Count and TF-IDF) were compared:
+- Logistic Regression
+- KNN
+- BernoulliNB
+- MultinomialNB
+- Random Forest Classifier
+- SVC (Support Vector Classifier)
+- AdaBoost Classifer
+
+The top models considered for hyperparameter tuning were:
+1. Logistic Regression
+2. Random Forest Classifier
+3. SVC
+4. AdaBoost Classifier
+
+**Table of scores of tuned models:**
+|  | Count Vectorizer | TD-IDF Vectorizer |
+|---|---:|---:|
+| **Logistic Regression** | Training: 0.98154<br>Testing: 0.90713<br>ROC_AUC: 0.88226 | Training: 0.90812<br>Testing:0.89746<br>ROC_AUC: 0.87036 |
+| **Random Forest Classifier** | Training: 0.99661<br>Testing: 0.89443<br>ROC_AUC: 0.87812 | Training: 0.99633<br>Testing: 0.90169<br>ROC_AUC: 0.88252 |
+| **Support Vector Classifier** | Training: 0.97728<br>Testing: 0.90371<br>ROC_AUC: 0.87972 | Training: 0.98373<br>Testing: 0.91156<br>ROC_AUC: 0.89170 |
+| **Ada Boost Classifier** | Training: 0.90534<br>Testing: 0.89705<br>ROC_AUC: 0.86792 | Training: 0.90703<br>Testing: 0.89142<br>ROC_AUC: 0.86241 |
+
+*NOTE: This table is the simplified version without the best parameters. Please refer to [eda_and_modeling.ipynb](https://git.generalassemb.ly/kluu/project-3/blob/master/code/eda_and_modeling.ipynb) for the table with tuned parameters.*
+
+**Through tuning, the best 2 models are:**
+1. **AdaBoost Classifier with Count Vectorization (lowest variance)**
+- Training: 0.90534
+- Testing: 0.89705
+- ROC_AUC: 0.86792
+
+
+- Parameters:
+    - stop_words = 'english'
+    - ngram_range = (1,2)
+    - DecisionTreeClassifier(max_depth=1)
+    - n_estimators = 200
+    - learning_rate = 0.5
+    
+    
+2. **SVC with TF-IDF Vectorization (highest ROC_AUC score + lowest bias)**
+- Training: 0.98373
+- Testing: 0.91156
+- ROC_AUC: 0.89170
+
+
+- Parameters:
+    - stop_words = stops (custom, extending English list)
+    - ngram_range = (1,2)
+    - kernel = sigmoid
+    - gamma = 0.1
+    - C = 10
+    
+    
+Some additional tuning was performed and it turns out that there was another SVC model that performed slightly better:
+<br>**Support Vector Classifier with TF-IDF Vectorizer**
+- Training: 0.98373
+- Testing: 0.91156
+- ROC_AUC: 0.89170
+
+
+- Parameters:
+    - kernal='linear'
+    - C=1
+    - gamma=1
+
+**Feature importance:**
+<br>While looking at the feature importances of each of the top 2 models, depending on the stop word treatment, different words and combinations of words had an impact on the models performance:
+1. AdaBoost + Count 
+- many combinations of games + "word" had an impact on the models performance in predicting the PC Gaming subreddit.
+    - this was to be expected the model was tuned to only remove the original 'English' stop words instead of the custom one
+
+
+2. SVC + TF-IDF
+- more distinctive single words have a heavier level of importance (along with some two word combinations that consist of the listed single words)
+    - since the custom stop words list was used for removal, we no longer see combinations of "games" + "words" compared to the previous list
+
+
+### CONCLUSION
+To achieve the goal of correctly predicting whether a post belongs to subreddit r/pcgaming or r/consoles, one of the two models can be utilized:
+1. **ADABOOST CLASSIFIER with COUNT VECTORIZATION**
+    - lowest variance for consistent predictions and best generalization
+    
+    
+2. **SUPPORT VECTOR CLASSIFER with TF-IDF VECTORIZATION**
+    - highest roc_auc for strong separability performance (and lowest bias)
+
+### RECOMENDATIONS
+1. Include lemmatization/stemming in the tuning process
+    - Potentially better scores may exist and the feature importances could narrow down
+2. Implement image processing due to many posts being image-only
+3. Re-conduct the analysis when the r/consoles community has expanded to have a minimum of 10000 posts
+    - Due to imblanced samples from each subreddit (more r/pcgaming vs. r/consoles)
---
+++ code/main_app.py
@@ -0,0 +1,308 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+
+from time import sleep
+from stqdm import stqdm # https://discuss.streamlit.io/t/stqdm-a-tqdm-like-progress-bar-for-streamlit/10097
+
+import matplotlib.pyplot as plt
+import os
+import re
+from bs4 import BeautifulSoup
+from pathlib import Path
+import pickle
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+
+import transformers
+from transformers import pipeline
+
+##################################################
+# Text cleaner
+##################################################
+
+def cleaner(text):
+	# Remove any HTML tags in-case
+	text = BeautifulSoup(text, features='lxml').get_text()
+
+	# Type and lower-cased
+	text = str(text)
+	text = text.lower()
+
+	# Replace "n't" with " not"
+	# Doesn’t, Isn’t, Wasn’t, Shouldn’t, Wouldn’t, Couldn’t, Won’t, Can’t, Don’t
+	text = re.sub(r"won\'t", "will not", text)
+	text = re.sub(r"can\'t", "can not", text)
+	text = re.sub(r"n\'t", " not", text)
+
+	# Join negatives "not" or "no" with next word
+	text = re.sub("not ", " NOT", text)
+	text = re.sub("no ", " NO", text)
+
+	# Remove non-alphabets
+	text = re.sub(r"[^A-Za-z0-9^,!?.\/'+]", " ", text)
+
+	# Extra cleaning beyond bs4
+	text = re.sub(r"\+", " plus ", text)
+	text = re.sub(r",", " ", text)
+	text = re.sub(r"\.", " ", text)
+	text = re.sub(r"!", " ! ", text)
+	text = re.sub(r"\?", " ? ", text)
+	text = re.sub(r"'", " ", text)
+	text = re.sub(r":", " : ", text)
+	text = re.sub(r"\s{2,}", " ", text)
+
+	return text
+
+##################################################
+# METHOD 1 - Simple Sentiment Analysis -- Single Review Input
+##################################################
+
+@st.cache(allow_output_mutation=True)
+def load_tokenizer():
+	with open('tokenizer/cnn_tokenizer.pickle', 'rb') as handle:
+		tokenizer = pickle.load(handle)
+	return tokenizer
+
+
+@st.cache(allow_output_mutation=True)
+def pre_model():
+
+	save_dest = Path('model')
+	save_dest.mkdir(exist_ok=True)
+	model_file = Path("model/model.hdf5")
+
+	if not model_file.exists():
+		with st.spinner("Downloading model... this may take awhile! \n Don't close or refresh!"):
+			cloud_id = '1Nuc9NEX-CvGMe2w5TG_LrHTcipIWTIGs'
+			from gdrive_downloader import download_file_from_google_drive
+			download_file_from_google_drive(cloud_id, model_file)
+    
+	model = load_model(model_file)
+	return model
+
+def predict(user_input):
+	cleaned_input = cleaner(user_input)
+	padded = pad_sequences(tokenizer.texts_to_sequences([cleaned_input]), 300)
+	pred = model.predict(padded)[0][0]
+	
+	st.write(f'{round(pred*100,2)}% POSITIVE')
+	st.write(f'{round((1-pred)*100,2)}% NEGATIVE')
+	
+	if pred > 0.7:
+		st.write('Conclusion: POSITIVE EXPERIENCE')
+		st.write("This client has experience an overall positive. Continue to deliver similar products and/or solutions that align with the positives in this review.")
+	elif pred > 0.3:
+		st.write('Conclusion: NEUTRAL EXPERIENCE')
+		st.write("Although this is not a negative experience, let's discuss about this experience to understand the areas of improvements. Please refer to our consultants at improvements@bestservice.ca. We would like to propose ")		
+	else:
+		st.write('Conclusion: NEGATIVE EXPERIENCE')
+		st.write("Negative experience are always a great learning opportunity. Let's figure out a solution that can turn this negative into a positive for the future. Please refer to our consultants at improvements@bestservice.ca. We're more than happy to assist you with mitigating negative experiences!")
+	
+def length(user_input):
+	words = user_input.split()
+	return len(words)
+
+##################################################
+# METHOD 2 - Complex Sentiment Analysis -- Collection of reviews
+##################################################
+
+def sentiment_pred(text):
+	# Possible Sentiment Categories
+	# Send the labels and tweets to the classifier pipeline
+	candidate_labels = ["positive", "neutral", "negative"]
+	result = classifier(text, candidate_labels)
+	
+	# Extract first label (predicted sentiment)
+	# As the classifier results is sorted in descending order (highest one first)
+	label = result['labels'][0]
+	
+	return label
+
+
+
+########################################################################################################################################################################################################
+# WEB-APP
+########################################################################################################################################################################################################
+st.set_page_config(layout="wide")
+page = st.sidebar.selectbox('Select page', ['Please select', 'Simple Analyzer','In-Depth Analyzer'])
+
+if page == 'Please select':
+	st.title("REVIEW ANALYSIS CONSULTATION SERVICES")
+	st.header("Accelerate your growth.")
+	st.markdown("With your client reviews, we can stategically develop innovative solutions to help accelerate positive changes & unlock your vision.")
+	
+##################################################
+# PAGE 1 - SIMPLE, SINGLE REVIEW INPUT
+##################################################
+# Single review page
+elif page == 'Simple Analyzer':
+	st.title("REVIEW ANALYSIS CONSULTATION SERVICES")
+	
+	# Load pre-train tokenizer and neural network model (highest accuracy)
+	tokenizer = load_tokenizer()
+	model = pre_model()
+	
+	st.header('Simple Analyzer')
+	st.markdown('Enter a review to analyze!')
+	review = st.text_area('15 words minimum')
+	
+	if not review:
+		st.warning('Please enter a review.')
+	else:
+		if length(review) < 15:
+			st.warning('Please enter a minimum 15 word review.')
+		else:
+			predict(review)
+
+##################################################
+# PAGE 2 - COMPLEX, REVIEW COLLECTION INPUT
+##################################################
+elif page == 'In-Depth Analyzer':
+	st.title("REVIEW ANALYSIS CONSULTATION SERVICES")
+	
+	# Download pipeline BART model
+	with st.spinner("Preparing analyzer... this may take awhile! \n Don't close or refresh!"):
+		classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+		
+	st.header('In-Depth Analyzer')
+	st.markdown("Upload your collection of reviews to analyze. _The reviews must be stored into a .csv file under a single column named 'text.'_")
+	
+	# Sample dataset
+	sample_review = pd.read_csv('../datasets/movie_review_sample.csv')
+	st.download_button("Sample review dataset",
+				data=sample_review.to_csv(index=False).encode('utf-8'),
+				file_name='movie_review_sample.csv',
+				mime='text/csv')
+	
+	st.markdown("For efficiency purposes, work with 50 reviews at a time.")
+	upload = st.file_uploader('Upload a file')
+	
+
+	
+	if not upload:
+		st.warning('Please upload a .csv file')
+	else:
+		st.spinner(f'You have selected {upload.name}. Loading file...')
+		stqdm.pandas() # initializing progress_apply function
+		
+		try:
+			df = pd.read_csv(upload)
+			with st.spinner("Loaded. Now cleaning..."):
+				df.text = df.text.apply(cleaner)
+				st.success("Cleaned & stored!")
+		except:
+			df = pd.DataFrame()
+			st.write("Your file is not formatted correctly. \n Please ensure that there's a column named 'text' - all lower-cased")
+			
+		sample_size = st.selectbox('Select sample size', ['Please select', 50, 100, 200, 400, 500, 'ALL'])
+		
+		################################################## 
+		# SENTIMENT DISTRIBUTIONS
+		##################################################
+		st.header("Sentiment Distribution")
+		
+		if sample_size == 'Please select':
+			df2 = pd.DataFrame()
+			st.write()
+		elif (sample_size == 'ALL') or (sample_size >= len(df)):
+			df2 = df.copy()		
+		elif type(sample_size) == int:
+			df2 = df.sample(n=5, ignore_index=True)
+			
+		if len(df2) > 0:
+			with st.spinner('Analyzing sentiment...'):
+				df2['sentiment'] = df2.text.progress_apply(sentiment_pred) # predict sentiment	
+			
+			frequency = df2.sentiment.value_counts() # extract sentiment value counts -- which is in descending order
+			labels = list(frequency.index) # sentiment labels
+			values = list(frequency) # frequencies of each sentiment
+		else:
+			labels = []
+			values = []
+			st.write()
+		
+		# 2 Columns
+		pie, reviews = st.columns(2)
+		
+		# Column 1 - donut-pie sentiment distirbution
+		with pie:	
+			# donut pie-chart
+			donut = go.Figure(data=[go.Pie(labels=[l.upper() for l in labels], values=values, hole=.3)])
+			donut.update_traces(marker = dict(colors=['#b9bfff','#fff9b9','#ffb9bf']))
+			st.plotly_chart(donut, use_container_width=True)
+				
+			if labels[0] == 'positive':
+				st.write("The majority of your reviews are __positive__. Let's identify common trends to consitently maintain positivity.")
+			elif labels[0] == 'negative':
+				st.write("The majority of your reviews are __negative__. Let's identify areas of opportunities for improvement.")
+			elif labels[0] == 'neutral':
+				st.write("The majority of your reviews are __neutral__. Let's observe the reviews to figure out how we can improve the positives while minimizing the negatives.")
+		
+		################################################## 
+		# LABELED DATAFRAME
+		##################################################
+		
+		# Color mapping created
+		color_map = {'negative':'#ffb9e2', 'positive':'#b9ffd6', 'neutral':'#ffd6b9'}
+		mapped_colors = list(df2["sentiment"].map(color_map))
+		fill_color = [mapped_colors for _ in range(len(df2.columns))]
+
+		# Column 2 - reviews with predicted sentiment
+		with reviews:
+			table = go.Figure(data=[go.Table(
+				header=dict(values=[f"<b>{col.upper()}</b>" for col in list(df2.columns)],
+					fill_color='#b9e2ff',
+					align='center'),
+				cells=dict(values=[df2.text, df2.sentiment],
+				       fill_color = fill_color,
+				       align='left'))
+				])
+			table.update_layout(width=1500, height=750)
+			st.plotly_chart(table, use_container_width=True)
+
+		
+		################################################## 
+		# CHARACTER & COUNT DISTRIBUTIONS
+		##################################################
+		st.header("Character and Word Count Distributions")
+		char, word = st.columns(2)
+		
+		with char:	
+			# Character count distribution	
+			df['char_count'] = df.text.str.len()
+			fig = px.histogram(df, x='char_count', color_discrete_sequence=['#ffb9bf'])
+			st.plotly_chart(fig, use_container_width=True)
+			st.write(f"Most reviews have around a __{df.char_count.mode()[0]}__ character count, but the overall average is about __{round(df.char_count.mean())}__ characters per review.")
+		
+		with word:
+			# Word count distribution
+			df['word_count'] = df.text.str.split().apply(len)
+			fig2 = px.histogram(df, x='word_count', color_discrete_sequence=['#b9bfff'])
+			st.plotly_chart(fig2, use_container_width=True)
+			st.write(f"Most reviews have around a __{df.word_count.mode()[0]}__ word count, but the overall average is about of __>{round(df.word_count.mean())}__ words per review.")
+		 
+		 
+			
+		
+			
+		
+
+
+
+
+
+
+
+	   
+
+
+    
+
